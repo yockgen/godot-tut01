161e938e669e2f301a735fba4621db500797041f
yockgen <yockgen@gmail.com>
2023-10-22 20:39:26 +0800

    Added .gitignore
    Signed-off-by: yockgen <yockgen@gmail.com>

--- main.gd
+++ main.gd
@@ -32,17 +32,12 @@ func _on_MobTimer_timeout():
 	mob.name="enemy"
 	
 	add_child(mob)
-	
+	spawn(mob)	
 
-	# Set the mob's direction perpendicular to the path direction.
+func spawn (obj): 
 	var direction = $MobPath/MobSpawnLocation.rotation + PI / 2
-	# Set the mob's position to a random location.
-	mob.position = $MobPath/MobSpawnLocation.position
-	# Add some randomness to the direction.
+	obj.position = $MobPath/MobSpawnLocation.position
 	direction += rand_range(-PI / 4, PI / 4)
-	mob.rotation = direction
-	# Set the velocity (speed & direction).
-	#print(mob.max_speed)
-	#mob.linear_velocity = Vector2(rand_range(mob.min_speed, mob.max_speed), 0)
-	mob.linear_velocity = Vector2(rand_range(150, 200), 0)
-	mob.linear_velocity = mob.linear_velocity.rotated(direction)
+	obj.rotation = direction
+	obj.linear_velocity = Vector2(rand_range(150, 200), 0)
+	obj.linear_velocity = obj.linear_velocity.rotated(direction)
